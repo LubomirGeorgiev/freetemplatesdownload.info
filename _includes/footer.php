aa7c17203ba8b5c0d1590fdb47db4e31776c7001
Lubomir Georgiev <lubomirgeorgievgeorgiev@gmail.com>
2014-03-27 16:06:53 +0200

    Updated the footer

--- _includes/footer.php
+++ _includes/footer.php
@@ -3,12 +3,6 @@
 <footer class="footer">
 	<div class="container">
 		<div class="row">
-			<div class="copyright col-md-12"><p>© 2014 Lubomir Georgiev (Любомир Георгиев), © 2014 freetemplatesdownload.info, All rights reserved.</p></div>
-		</div>
-		<div class="row">
-			<div class="col-md-12"><p class="text-center">If you have any questions, please do not hesitate to contact me: <b>lubomirgeorgievgeorgiev [at] gmail.com</b></p></div>
-		</div>
-		<div class="row">
 			<div class="footerpanel clearfix">
 				<?php echo breadcrumbs(); ?>
 					<ul class="footlinks clearfix">
@@ -19,16 +13,26 @@
 						<li><a href="http://freetemplatesdownload.info/template/yourflatportfolio.html?utm_source=Footer&utm_medium=referral&utm_campaign=Footer+to+Template">Your Flat Portfolio</a></li>
 						<li><a href="http://freetemplatesdownload.info/template/purity.html?utm_source=Footer&utm_medium=referral&utm_campaign=Footer+to+Template">Purity</a></li>
 						<li><a href="http://freetemplatesdownload.info/blog/?utm_source=Footer&utm_medium=referral&utm_campaign=Footer+to+blog">Blog</a></li>
-						<li><a href="http://getbootstrap.com/">Built with Bootstrap</a></li>
+						<li><a href="http://getbootstrap.com/" title="HUGE thanks to the creators of bootstrap and all the contributors in the community.">Built with Bootstrap</a></li>
 						<li><a href="http://freetemplatesdownload.info/sitemap.html?utm_source=Footer&utm_medium=referral&utm_campaign=Footer+to+sitemap">Sitemap</a></li>
 					</ul>
 			</div>
 		</div>
-		<div class="row" style="margin-top: 40px;">
+		<div class="row footer-info">
+				<div class="copyright">
+					<div><p>© 2014 Lubomir Georgiev (Любомир Георгиев), © 2014 freetemplatesdownload.info, All rights reserved.</p>
+					<p class="text-center">If you have any questions, please do not hesitate to contact me: <b>lubomirgeorgievgeorgiev [at] gmail.com</b></p>
+					</div>
+				</div>
+
 			<div class="col-md-6 col-sm-6 made-in-bulgaria">
-				<p>Made with <span class="glyphicon glyphicon-heart"></span> in City of Sofia, Bulgaria. <a rel="nofollow" href="http://en.wikipedia.org/wiki/Bulgaria"><span class="bulgaria" alt="Bulgaria, България"></span></a></p>
+				<p>Made with <span class="glyphicon glyphicon-heart"></span> in City of Sofia, Bulgaria. <a rel="nofollow" title="I am proud to be a bulgarian!!!" href="http://en.wikipedia.org/wiki/Bulgaria"><span class="bulgaria" alt="Bulgaria, България"></span></a></p>
 			</div>
-			<div class="col-md-6 col-sm-6"><blockquote><p>Simplicity is the ultimate sophistication.</p> <span>- Leonardo da Vinci</span></blockquote></div>
+				<div class="col-md-6 col-sm-6"><blockquote><p>Simplicity is the ultimate sophistication.</p> <span>- Leonardo da Vinci</span></blockquote></div>
+				<div class="col-md-6 col-md-push-3 signature">
+				<img title="Email: lubomirgeorgievgeorgiev@gmail.com" src="http://freetemplatesdownload.info/images/signature.svg" class="img-responsive" alt="Lubomir Georgiev's signature">
+				</div>
+			</div>
 		</div>
 	</div>
 </footer>
