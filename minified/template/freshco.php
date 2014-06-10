054855a1dc73440be0bea28330acdf74188a4a4b
LubomirGeorgiev <lubomirgeorgievgeorgiev@gmail.com>
2014-06-10 16:00:54 +0300

    Clean branch "jekyll-template-framework"

--- minified/template/freshco.php
+++
@@ -1,9 +0,0 @@
-<!--
-**** This file has been minified, compressed, shortened and squeezed :) in order to improve the efficiency and reduce the traffic.
-**** The original source code of the file is available at https://github.com/LubomirGeorgiev/freetemplatesdownload.info
-
-**** Copyright 2014 Lubomir Georgiev email: lubomirgeorgievgeorgiev@gmail.com
-**** Licensed under - Creative Commons Attribution 4.0 International (http://creativecommons.org/licenses/by/4.0/)
--->
-
-<!DOCTYPE html><html lang="en"><head><title>FreshCO - A Free Web Template | FreeTemplatesDownload.info</title><?php include '../_includes/meta.php'; ?><link href="../css/global.css<?php include '../_includes/stylesheet_version.php'; ?>" rel="stylesheet" media="screen"><?php include '../_includes/google-analytics.php'; ?></head><body class="page template-review-single"><?php include '../_includes/main-nav.php'; ?><div class="container"><div class="template-details clearfix"><a href="http://freetemplatesdownload.info/livepreview/freshco/"><h1>FreshCO</h1><h4>A free, fresh looking web templatertists as a portfolio page.</h4><div class="promobillboard"><img alt="FreshCo is a free web template. - Fresh looking, Fully responsible, Beautiful, Full Screen Slider, Multipurpose use, Mobile friendly, Beautiful design" class="img-responsive" src="http://freetemplatesdownload.info/images/promobillboards/freshco.jpg"></div></a><div class="promobuttons"><a class="btn btn-danger button" href="http://freetemplatesdownload.info/download/freshco.zip">Download</a> <a class="btn btn-success button" href="http://freetemplatesdownload.info/livepreview/freshco/">Live preview</a></div></div></div><?php include '../_includes/footer.php'; ?><script src="../bootstrap/js/bootstrap.min.js"></script></body></html>
--- template/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-   header( 'Location: http://www.freetemplatesdownload.info/all-templates.html' ) ;
-?>
